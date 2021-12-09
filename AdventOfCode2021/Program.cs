cbbfbcf8eb8f51988ac61298d4e0277bcde5124e
Pete Forrest <peter.forrest@abstractleap.com>
2021-12-09 15:25:35 +0000

    Day 8 & 9

--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -18,5 +18,11 @@
 //Day6.Part1(Day6.Input);
 //Day6.Part2(Day6.Input);
 
-Day7.Part1(Day7.Input);
-Day7.Part2(Day7.Input);
+//Day7.Part1(Day7.Input);
+//Day7.Part2(Day7.Input);
+
+//Day8.Part1(Day8.Input);
+//Day8.Part2(Day8.Input);
+
+Day9.Part1(Day9.Input);
+Day9.Part2(Day9.Input);
